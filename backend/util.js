43ff3120b28f2489fc445fb7711b10c406c96641
saurabhsls <57672690+saurabhsls@users.noreply.github.com>
2020-12-08 19:43:02 +0530

    Update util.js

--- backend/util.js
+++ backend/util.js
@@ -15,30 +15,6 @@ const getToken = (user) => {
   );
 };
 
-const isAuth = (req, res, next) => {
-  const token = req.headers.authorization;
 
-  if (token) {
-    const onlyToken = token.slice(7, token.length);
-    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
-      if (err) {
-        return res.status(401).send({ message: 'Invalid Token' });
-      }
-      req.user = decode;
-      next();
-      return;
-    });
-  } else {
-    return res.status(401).send({ message: 'Token is not supplied.' });
-  }
-};
 
-const isAdmin = (req, res, next) => {
-  console.log(req.user);
-  if (req.user && req.user.isAdmin) {
-    return next();
-  }
-  return res.status(401).send({ message: 'Admin Token is not valid.' });
-};
-
-export { getToken, isAuth, isAdmin };
+export { getToken };
