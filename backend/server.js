1e4b7cdfb1ccff180d6a8b3f2be901abd0ca6b9f
saurabhsls <57672690+saurabhsls@users.noreply.github.com>
2020-12-08 19:41:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,36 @@
 import express from 'express';
 import data from './data';
-import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute';
+import dotenv from 'dotenv';
 
 dotenv.config();
-
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
-    useNewUrlParser: true
-}).catch(error => console.log(error.reason));
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }).catch(error => console.log(error.reason));
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
+app.get("/api/products/:id",(req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x=>x._id === productId);
+    if(product)
+        res.send(product);
+    else{
+        res.status(404).send({msg: "Product Not Found." });
+    }
+
+});
+
 app.get("/api/products",(req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(5007, () => {console.log('Server started at http://localhost:5007')});
