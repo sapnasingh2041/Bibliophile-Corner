c03991257ebf1ed88fcd4efd952af098fc7ffe13
saurabhsls <57672690+saurabhsls@users.noreply.github.com>
2020-12-08 20:08:02 +0530

    Update userReducers.js

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -27,16 +27,17 @@ function userUpdateReducer(state = {}, action) {
 }
 
 function userRegisterReducer(state = {}, action) {
-  switch (action.type) {
-    case USER_REGISTER_REQUEST:
-      return { loading: true };
-    case USER_REGISTER_SUCCESS:
-      return { loading: false, userInfo: action.payload };
-    case USER_REGISTER_FAIL:
-      return { loading: false, error: action.payload };
-    default: return state;
+    switch (action.type) {
+      case USER_REGISTER_REQUEST:
+        return { loading: true };
+      case USER_REGISTER_SUCCESS:
+        return { loading: false, userInfo: action.payload };
+      case USER_REGISTER_FAIL:
+        return { loading: false, error: action.payload };
+      default: return state;
+    }
   }
-}
+
 export {
-  userSigninReducer, userRegisterReducer, userUpdateReducer
+    userSigninReducer, userRegisterReducer, userUpdateReducer
 }
