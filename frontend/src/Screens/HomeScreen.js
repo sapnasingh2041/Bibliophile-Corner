657a2cbe1750ff30afcb70a64e41417f6b560672
saurabhsls <57672690+saurabhsls@users.noreply.github.com>
2020-12-08 19:54:44 +0530

    Update HomeScreen.js

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
-
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen (props) {
-    return <ul className="products">
+   const productList = useSelector(state => state.productList);
+   const { products, loading, error } = productList;
+   const dispatch = useDispatch(); 
+   useEffect(() => { 
+       dispatch(listProducts());         
+
+       return() => {
+        //
+       };
+   }, [])
+
+    return loading? <div>Loading...</div> :
+    error? <div>{error}</div>:
+    <ul className="products">
         {
-          data.products.map(product => 
-          <li>
+          products.map(product => 
+          <li key={product._id}>
            <div className="product">
         <Link to={'/product/' + product._id}>
            <img className="product-image" src={product.image} alt="product"></img>
