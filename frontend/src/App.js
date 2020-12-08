9390ae2e3ec9b5bdff9cf40deaf56fc84d8467b6
saurabhsls <57672690+saurabhsls@users.noreply.github.com>
2020-12-08 20:10:01 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import data from './data';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import SigninScreen from './screens/SigninScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
-
+  
   const userSignin = useSelector(state=>state.userSignin);
   const {userInfo} = userSignin;
 
@@ -27,14 +31,26 @@ function App() {
               <Link to="/" > BiblioPhile Corner</Link>
                 </div>
               <div className="header-links">
-                    <a href="cart.html"> Cart</a>
-                    {
-                      userInfo ? <Link to="/profile">{userInfo.name}</Link>
-                    ) : (
-                    <Link to="/signin"></Link>
-                    )}
-                    <a href="signin.html"> Sign In</a>
+                    <a href="/cart"> Cart</a>
+            
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}                    
+
+          {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
               </div>
+            )}
+            </div>
           </header>
         
             <aside className="sidebar">
@@ -51,12 +67,13 @@ function App() {
             </aside>
         <main className="main">
             <div className="content">
+              <Route path="/profile" component={ProfileScreen} />
               <Route path="/product/:id" component={ProductScreen}/>
               <Route path="/signin" component={SigninScreen} />
               <Route path="/register" component={RegisterScreen} />
-              <Route path="/cart/:id?" component={CartScreen} />
+              <Route path="/cart/:id?" component={CartScreen}/>
               <Route path="/" exact={true} component={HomeScreen}/>
-                </div>
+            </div>
                 
             </main>
 
