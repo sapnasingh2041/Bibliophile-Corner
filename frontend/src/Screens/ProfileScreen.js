a8d7549a11589b17fd736e0c808659255d97ab22
saurabhsls <57672690+saurabhsls@users.noreply.github.com>
2020-12-09 09:40:34 +0530

    Create ProfileScreen.js

---
+++ frontend/src/Screens/ProfileScreen.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { logout, update } from '../actions/userActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+function ProfileScreen(props) {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const dispatch = useDispatch();
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+  const handleLogout = () => {
+    dispatch(logout());
+    props.history.push("/signin");
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(update({ userId: userInfo._id, email, name, password }))
+  }
+  
+  
+
+  return <div className="profile">
+    <div className="profile-info">
+      <div className="form">
+        <form onSubmit={submitHandler} > 
+          <ul className="form-container">
+            <li>
+              <h2>User Profile</h2>
+            </li>
+            
+            <li>
+              <label htmlFor="name">
+                Name
+          </label>
+              <input value={name} type="name" name="name" id="name" onChange={(e) => setName(e.target.value)}>
+              </input>
+            </li>
+            <li>
+              <label htmlFor="email">
+                Email
+          </label>
+              <input value={email} type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}>
+              </input>
+            </li>
+            <li>
+              <label htmlFor="password">Password</label>
+              <input value={password} type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}>
+              </input>
+            </li>
+
+            <li>
+              <button type="submit" className="button primary">Update</button>
+            </li>
+            <li>
+              <button type="button" onClick={handleLogout} className="button secondary full-width">Logout</button>
+            </li>
+
+          </ul>
+        </form>
+      </div>
+    </div>
+
+  </div>
+}
+
+export default ProfileScreen;
